477103239dbd6faf1875f46e5e83164ce22a8236
Blaineworld <blainephargett@gmail.com>
2019-09-15 19:30:54 -0500

    Added initializer.js.

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 'use strict';
 
-;
+const I = new (require("./lib/initializer.js"))(1, function() {
+	console.log("Finished!");
+
+	// Minecraft . . .
+});
+
+const fs = require("fs");
+
+fs.readFile("server.properties", "utf8", async function(error, data) {
+	// Process it . . .
+
+	// Tick it off!
+	I.tick();
+});
